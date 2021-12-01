5eefeba25a31bb785d30bfc2ab581cda17ce2f90
Karol Krizka <kkrizka@gmail.com>
2021-11-30 22:43:44 -0800

    Fix ACTSTracking_version.

--- releases/development/release-versions.py
+++ releases/development/release-versions.py
@@ -251,7 +251,7 @@ Physsim_version = "v00-04-01"
 
 ACTS_version = "v13.0.0"
 
-ACTSTracking_version = "main"
+ACTSTracking_version = "HEAD"
 
 # xerces-c (needed by geant4 for building gdml support - required by mokka)
 XercesC_version = "v3.2.2"
